4172c16c6e5e88e527a071b5febf8399aefc2367
pietrodn <powerpdn@gmail.com>
2017-01-26 11:25:51 +0100

    More elegant implementation of Mapper (Java)

--- java-threads/Mapper/src/it/pietrodn/mw/mapper/Mapper.java
+++ java-threads/Mapper/src/it/pietrodn/mw/mapper/Mapper.java
@@ -1,101 +1,108 @@
 package it.pietrodn.mw.mapper;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Mapper {
-	
-	private Thread[] threads;
+
 	private PartialMapper[] pms;
 	private int nthreads;
-	
+
 	public Mapper(int nthreads) {
-		threads = new Thread[nthreads];
-		pms = new PartialMapper[nthreads];
+
 		this.nthreads = nthreads;
-		
+		pms = new PartialMapper[nthreads];
+
 		// Setup and start the threads
-		for(int i=0; i<nthreads; i++) {
+		for (int i = 0; i < nthreads; i++) {
 			pms[i] = new PartialMapper();
-			threads[i] = new Thread(pms[i]);
-			threads[i].start();
+			pms[i].start();
 		}
 	}
-	
+
 	public void map(int[] data, Processor p) {
 		int i, j;
-		
-		// Set the processor
-		for(j=0; j < nthreads; j++) {
-			pms[j].setProc(p);
-		}
-		
+
+		System.out.printf("Running with %d threads.\n", nthreads);
+
 		// Feed the data
-		for(i=0; i < data.length; i++) {
+		for (i = 0; i < data.length; i++) {
 			j = i % nthreads;
-			pms[j].addElement(data[i]);
+			pms[j].addElement(data[i], p);
 		}
-		
+
 		// Retrieve the results
-		for(i=0; i < data.length; i++) {
+		for (i = 0; i < data.length; i++) {
 			j = i % nthreads;
 			data[i] = pms[j].getResult();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		Mapper mapper = new Mapper(4);
+		Mapper mapper = new Mapper(11);
 		Processor proc = new ProcessorImpl();
-		
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		mapper.map(arr, proc);
-		
-		for(int i=0; i<arr.length; i++) {
+
+		for (int i = 0; i < arr.length; i++) {
 			System.out.printf("%d\t", arr[i]);
 		}
-		
+
 		System.out.println("");
-		
+
 		mapper.map(arr, proc);
-		
-		for(int i=0; i<arr.length; i++) {
+
+		for (int i = 0; i < arr.length; i++) {
 			System.out.printf("%d\t", arr[i]);
 		}
-		
+
+		// Kill all the threads that are left running
 		System.exit(0);
 
 	}
-	
-	class PartialMapper implements Runnable {
-		
+
+	class PartialMapper extends Thread {
+
+		// ProcItem represent a couple (int, Processor) to process
+		class ProcItem {
+			private int val;
+			private Processor p;
+
+			public ProcItem(int val, Processor p) {
+				this.val = val;
+				this.p = p;
+			}
+
+			// Method to get the processed value
+			public int getResult() {
+				return p.process(val);
+			}
+		}
+
 		Processor proc;
-		
-		LinkedList<Integer> elems;
-		LinkedList<Integer> results;
-		
+
+		// Queues for inputs and output
+		Queue<ProcItem> inQueue;
+		Queue<Integer> results;
+
 		public PartialMapper() {
-			elems = new LinkedList<Integer>();
+			inQueue = new LinkedList<ProcItem>();
 			results = new LinkedList<Integer>();
 		}
-		
-		public synchronized void setProc(Processor proc) {
-			// Set the processor. This resets elems and results.
-			this.proc = proc;
-			elems = new LinkedList<Integer>();
-			results = new LinkedList<Integer>();
+
+		public synchronized void addElement(int e, Processor p) {
+			inQueue.add(new ProcItem(e, p));
+			notifyAll();
 		}
-		
-		public synchronized void addElement(int e) {
-			elems.add(e);
-			notify();
-		}
-		
+
 		@Override
 		public void run() {
-			while(true) {
-				synchronized(this) {
-					
+			while (true) {
+				synchronized (this) {
+
 					// Waits for elements in the buffer and processes them
-					while(elems.isEmpty()) {
+					while (inQueue.isEmpty()) {
 						try {
 							wait();
 						} catch (InterruptedException e) {
@@ -103,17 +110,20 @@ public class Mapper {
 							e.printStackTrace();
 						}
 					}
-					while(!elems.isEmpty()) {
-						results.add(proc.process(elems.pop()));
+					while (!inQueue.isEmpty()) {
+						results.add(inQueue.poll().getResult());
+
+						// Notify after each processed item to allow to
+						// interleave reads
+						notifyAll();
 					}
-					notifyAll();
 				}
 			}
 		}
-		
+
 		public synchronized int getResult() {
 			// Wait for a result and return it
-			while(results.isEmpty()) {
+			while (results.isEmpty()) {
 				try {
 					wait();
 				} catch (InterruptedException e) {
@@ -121,7 +131,7 @@ public class Mapper {
 					e.printStackTrace();
 				}
 			}
-			return results.pop();
+			return results.poll();
 		}
 	}
 
