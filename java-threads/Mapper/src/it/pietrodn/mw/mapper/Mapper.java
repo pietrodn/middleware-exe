5b97788f85f9fcf54c459524cc3eb9a381eb79cb
pietrodn <powerpdn@gmail.com>
2017-01-26 11:27:15 +0100

    Moved PartialMapper to a separate file

--- java-threads/Mapper/src/it/pietrodn/mw/mapper/Mapper.java
+++ java-threads/Mapper/src/it/pietrodn/mw/mapper/Mapper.java
@@ -1,8 +1,5 @@
 package it.pietrodn.mw.mapper;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Mapper {
 
 	private PartialMapper[] pms;
@@ -61,78 +58,4 @@ public class Mapper {
 		System.exit(0);
 
 	}
-
-	class PartialMapper extends Thread {
-
-		// ProcItem represent a couple (int, Processor) to process
-		class ProcItem {
-			private int val;
-			private Processor p;
-
-			public ProcItem(int val, Processor p) {
-				this.val = val;
-				this.p = p;
-			}
-
-			// Method to get the processed value
-			public int getResult() {
-				return p.process(val);
-			}
-		}
-
-		Processor proc;
-
-		// Queues for inputs and output
-		Queue<ProcItem> inQueue;
-		Queue<Integer> results;
-
-		public PartialMapper() {
-			inQueue = new LinkedList<ProcItem>();
-			results = new LinkedList<Integer>();
-		}
-
-		public synchronized void addElement(int e, Processor p) {
-			inQueue.add(new ProcItem(e, p));
-			notifyAll();
-		}
-
-		@Override
-		public void run() {
-			while (true) {
-				synchronized (this) {
-
-					// Waits for elements in the buffer and processes them
-					while (inQueue.isEmpty()) {
-						try {
-							wait();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-					while (!inQueue.isEmpty()) {
-						results.add(inQueue.poll().getResult());
-
-						// Notify after each processed item to allow to
-						// interleave reads
-						notifyAll();
-					}
-				}
-			}
-		}
-
-		public synchronized int getResult() {
-			// Wait for a result and return it
-			while (results.isEmpty()) {
-				try {
-					wait();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			return results.poll();
-		}
-	}
-
 }
